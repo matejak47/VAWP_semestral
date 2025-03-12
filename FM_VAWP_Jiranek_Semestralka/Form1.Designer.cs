e762485180fdfe4c2302622153a8aa9b301a1931
Matěj Jiránek <145572111+matejak47@users.noreply.github.com>
2025-03-12 13:27:33 +0100

    data

--- FM_VAWP_Jiranek_Semestralka/Form1.Designer.cs
+++ FM_VAWP_Jiranek_Semestralka/Form1.Designer.cs
@@ -28,19 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            listView1 = new ListView();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(listView1, 0, 0);
+            tableLayoutPanel1.Location = new Point(0, -3);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(548, 808);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Location = new Point(554, -3);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(535, 808);
+            tableLayoutPanel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 1;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Location = new Point(1095, -3);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 1;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Size = new Size(436, 808);
+            tableLayoutPanel3.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(3, 3);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(542, 802);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1543, 805);
+            Controls.Add(tableLayoutPanel3);
+            Controls.Add(tableLayoutPanel2);
+            Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel3;
+        private ListView listView1;
     }
 }
--- FM_VAWP_Jiranek_Semestralka/Form1.cs
+++ FM_VAWP_Jiranek_Semestralka/Form1.cs
@@ -1,4 +1,5 @@
 using FM_VAWP_Jiranek_Semestralka.Data;
+using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 
 namespace FM_VAWP_Jiranek_Semestralka
@@ -10,10 +11,34 @@ namespace FM_VAWP_Jiranek_Semestralka
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {var context = new VAPW_PS_DrivesContext(ConfigurationManager.ConnectionStrings["Pripojeni"].ConnectionString);
-         var data = context.DriveData.ToList();
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var context = new VAPW_PS_DrivesContext(ConfigurationManager.ConnectionStrings["Pripojeni"].ConnectionString);
+            var data = context.DriveData.ToList();
+            listView1.View = View.Details; 
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Columns.Add("ID", 50);
+            listView1.Columns.Add("Recording ID", 100);
+            listView1.Columns.Add("Lat", 100);
+            listView1.Columns.Add("Lon", 100);
+            listView1.Columns.Add("Speed", 100);
+            var list = await context.DriveData.Take(100).ToListAsync();
 
+            foreach (var item in list)
+            {
+                ListViewItem row =new ListViewItem(item.Id.ToString());
+                row.SubItems.Add(item.RecordingId.ToString());
+                row.SubItems.Add(item.Lat.ToString());
+                row.SubItems.Add(item.Lon.ToString());
+                row.SubItems.Add(item.Speed.ToString());
+                listView1.Items.Add(row);
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
